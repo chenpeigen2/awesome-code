27d16575517519bf76af529ea3d864fdc16ad22e
chenpeigen <chenpeigen@xiaomi.com>
2023-04-05 14:21:06 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("java")
-    kotlin("jvm") version "1.8.10"
+    kotlin("jvm") version "1.8.20"
 }
 
 group = "org.peter"
--- src/main/java/featuretest/extend/BASE.java
+++ src/main/java/featuretest/extend/BASE.java
@@ -1,17 +1,48 @@
 package featuretest.extend;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Base implements LifeCycleOwner {
     public String mLifecycleRegistry = new String("abc");
+
+    public String mLifecycleRegistry3 = new String("abc");
+
+    protected Map<String, String> headers = new HashMap<>() {
+        {
+            put("a", "b");
+//            System.out.println(this == null);
+            System.out.println(headers == null);
+        }
+    };
+
+    {
+        System.out.println("566666666666666");
+        System.out.println(headers == null);
+    }
+
+    public String mLifecycleRegistry1 = new String("abc");
 
     @Override
     public String getLifeCycle() {
         return mLifecycleRegistry;
     }
+
+    public static Base getInstance() {
+        return new SUB();
+    }
 }
 
 
-class SUB extends Base implements LifeCycleOwner{
-     String mLifecycleRegistry = new String("bde");
+class SUB extends Base implements LifeCycleOwner {
+    String mLifecycleRegistry = new String("bde");
+
+    int a = 5678;
+
+    public SUB() {
+        super();
+        System.out.println("222222222");
+    }
 
     @Override
     public String getLifeCycle() {
@@ -19,7 +50,23 @@ class SUB extends Base implements LifeCycleOwner{
     }
 
     public static void main(String[] args) {
-        var app = new SUB();
-        System.out.println(app.getLifeCycle());
+        var app = new SUB() {{
+            System.out.println(this);
+            System.out.println("5567");
+            System.out.println(headers == null);
+        }};
+//        System.out.println(app.getLifeCycle());
+//        SUB sub = new SUB();
+    }
+}
+
+class CC {
+    protected Map<String, String> map = new HashMap<>() {{
+        put("we", "are");
+        map.put("s", "5");
+    }};
+
+    public static void main(String[] args) {
+        new CC();
     }
 }
