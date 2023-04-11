4981f9c55afe7d1ec31a428004a98b0171e8460c
chenpeigen <chenpeigen@xiaomi.com>
2023-04-11 21:16:39 +0800

    lee-20230411

---
+++ src/main/java/lee/pkg20230411/Solution.java
@@ -0,0 +1,27 @@
+package lee.pkg20230411;
+
+public class Solution {
+    //    https://leetcode.cn/problems/robot-bounded-in-circle/
+    public boolean isRobotBounded(String instructions) {
+        // 北 东 南 西 （顺时针）
+        int[][] direc = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int direcIndex = 0;
+        int x = 0;
+        int y = 0;
+        int n = instructions.length();
+        for (int idx = 0; idx < n; idx++) {
+            char instruction = instructions.charAt(idx);
+            if (instruction == 'G') {
+                x += direc[direcIndex][0];
+                y += direc[direcIndex][1];
+            } else if (instruction == 'L') {
+                direcIndex += 3;
+                direcIndex %= 4;
+            } else {
+                direcIndex++;
+                direcIndex %= 4;
+            }
+        }
+        return direcIndex != 0 || (x == 0 && y == 0);
+    }
+}
