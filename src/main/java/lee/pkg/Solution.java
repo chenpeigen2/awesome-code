379359ba9c538803194cbf30642370f478542bef
chenpeigen <chenpeigen@xiaomi.com>
2023-04-12 16:47:13 +0800

    add lee

--- src/main/java/lee/pkg/Solution.java
+++ src/main/java/lee/pkg/Solution.java
@@ -134,6 +134,25 @@ public class Solution {
 
     }
 
+    //    https://leetcode.cn/problems/longest-common-prefix/solution/zui-chang-gong-gong-qian-zhui-by-leetcode-solution/
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int length = strs[0].length();
+        int count = strs.length;
+        for (int i = 0; i < length; i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < count; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+
+
     public static void main(String[] args) {
         var app = new Solution();
 //        int[] arr = new int[]{1, 4, 2, 5, 3};
