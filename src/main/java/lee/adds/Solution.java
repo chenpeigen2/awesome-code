6ee706cd0d53d34cf58abd11913c8268a2fc8355
chenpeigen <chenpeigen@xiaomi.com>
2023-04-17 22:36:39 +0800

    add lee

--- src/main/java/lee/adds/Solution.java
+++ src/main/java/lee/adds/Solution.java
@@ -244,6 +244,15 @@ public class Solution {
         return l & r;
     }
 
+    //    https://leetcode.cn/problems/single-number/
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int num : nums) {
+            ans ^= num;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         var app = new Solution();
         int[] arr = new int[]{-1, 0, 1, 2, -1, -4};
