b076e3b6b4220019bac3d29d76653efa14ce1d52
chenpeigen <chenpeigen@xiaomi.com>
2023-04-27 19:01:31 +0800

    lee-20230427

---
+++ src/main/java/lee/pkg20230427/Solution.java
@@ -0,0 +1,26 @@
+package lee.pkg20230427;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    //    https://leetcode.cn/problems/longest-string-chain/
+    public int longestStrChain(String[] words) {
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        Map<String, Integer> cnt = new HashMap<String, Integer>();
+        int res = 0;
+        for (String word : words) {
+            cnt.put(word, 1);
+            for (int j = 0; j < word.length(); j++) {
+                String w = word.substring(0, j) + word.substring(j + 1);
+                if (cnt.containsKey(w)) {
+                    cnt.put(word, Math.max(cnt.get(word), cnt.get(w) + 1));
+                }
+            }
+            res = Math.max(res, cnt.get(word));
+        }
+        return res;
+    }
+}
