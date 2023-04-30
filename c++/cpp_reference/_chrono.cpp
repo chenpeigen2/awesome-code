74e338e2888d7fcffb35991f572539fba00bf561
chenpeigen <chenpeigen@xiaomi.com>
2023-04-30 11:21:04 +0800

    add chrono-1

--- c++/cpp_reference/_chrono.cpp
+++ c++/cpp_reference/_chrono.cpp
@@ -7,22 +7,30 @@
 #include "_main.h"
 
 
+#define ELAPSE_SECONDS 3
+
+auto refresh_time = std::chrono::seconds(ELAPSE_SECONDS); // 定义刷新时间间隔
+
+std::chrono::time_point<std::chrono::steady_clock, std::chrono::nanoseconds> start;
+
+void refreshIfNeeded() {
+    auto now = std::chrono::steady_clock::now(); // 获取当前时间点
+    if (start.time_since_epoch().count() == 0) {
+        start = now;
+    }
+    auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(now - start); // 计算时间差
+    if (elapsed_time >= refresh_time) { // 如果达到刷新时间间隔
+        std::cout << "5分钟已经过去了，重新刷新！"
+                  << std::chrono::time_point_cast<std::chrono::seconds>(now).time_since_epoch().count()
+                  << std::endl; // 输出消息
+        start = now; // 更新起始时间点
+    }
+}
+
+
 void test_chrono() {
-    auto start = std::chrono::steady_clock::now(); // 获取当前时间点
-    auto refresh_time = std::chrono::seconds(3); // 定义刷新时间间隔
-
     while (true) {
-        auto now = std::chrono::steady_clock::now(); // 获取当前时间点
-        auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(now - start); // 计算时间差
-
-        if (elapsed_time >= refresh_time) { // 如果达到刷新时间间隔
-            std::cout << "5分钟已经过去了，重新刷新！" << std::endl; // 输出消息
-            start = now; // 更新起始时间点
-        }
-
-        // 执行其他操作
-        // ...
-
+        refreshIfNeeded();
         std::this_thread::sleep_for(std::chrono::milliseconds(100)); // 等待一段时间
     }
 }
