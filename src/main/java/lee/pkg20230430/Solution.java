b223641201645320fd0f5be127097320fac31a7e
chenpeigen <chenpeigen@xiaomi.com>
2023-04-30 21:51:07 +0800

    lee-20230430

---
+++ src/main/java/lee/pkg20230430/Solution.java
@@ -0,0 +1,25 @@
+package lee.pkg20230430;
+
+public class Solution {
+    //    https://leetcode.cn/problems/moving-stones-until-consecutive/
+    public int[] numMovesStones(int a, int b, int c) {
+        int x = Math.min(a, Math.min(b, c));
+        int z = Math.max(a, Math.max(b, c));
+        int y = a + b + c - x - z;
+
+        int[] res = new int[2];
+        res[0] = 2;
+        if (z - y == 1 && y - x == 1) res[0] = 0;
+        else if (z - y <= 2 || y - x <= 2) res[0] = 1;  // 2 4 6 => 4 5 6
+        res[1] = z - x - 1 - 1;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        var app = new Solution();
+        var ans = app.numMovesStones(1, 5, 8);
+        for (int i : ans) {
+            System.out.println(i);
+        }
+    }
+}
