d38ceef7a95c496df85c27e3d29f9cecd5a7992e
chenpeigen <chenpeigen@xiaomi.com>
2023-05-14 22:39:52 +0800

    add cpp

--- c++/cpp_reference/main.cpp
+++ c++/cpp_reference/main.cpp
@@ -37,8 +37,29 @@
 //        }
 //    });
 
+double vals[] = {10.1, 12.6, 33.1, 24.1, 50.0};
+
+double &setValues(int i) {
+    double &ref = vals[i];
+    return ref;   // 返回第 i 个元素的引用，ref 是一个引用变量，ref 引用 vals[i]
+
+
+}
+
+using namespace std;
 
 int main() {
+
+    cout << "改变前的值" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "vals[" << i << "] = ";
+        cout << vals[i] << endl;
+    }
+
+    setValues(1) = 20.23; // 改变第 2 个元素
+    setValues(3) = 70.8;  // 改变第 4 个元素
+    double &zz = setValues(3);
+    zz = 567;
 
     {
 //        test_vec();
