985f96e1f01ed05b7682b197394468e750413a8b
chenpeigen <chenpeigen@xiaomi.com>
2023-05-21 23:47:17 +0800

    lee-20230521

--- src/main/java/lee/pkg20230521/Solution.java
+++ src/main/java/lee/pkg20230521/Solution.java
@@ -6,17 +6,17 @@ public class Solution {
 
     //    https://leetcode.cn/problems/o8SXZn/
     public int storeWater(int[] bucket, int[] vat) {
-        int n = bucket.length;
-        int maxk = Arrays.stream(vat).max().getAsInt();
-        if (maxk == 0) return 0;
+        int n = vat.length;
+        int maxK = Arrays.stream(vat).max().getAsInt();
+        if (maxK == 0) return 0;
         int res = Integer.MAX_VALUE;
-
-        for (int k = 1; k <= maxk && k < res; ++k) {
+        for (int k = 1; k <= maxK && k < res; k++) {
             int t = 0;
 
-            for (int i = 0; i < bucket.length; i++) {
-                t += Math.max(0, (vat[i] + k - 1) / k - bucket[i]);
+            for (int i = 0; i < n; i++) {
+                t += Math.max(0, (vat[i] - 1 + k) / k - bucket[i]); // 需要升级的bucket的容量
             }
+
             res = Math.min(res, t + k);
         }
 
