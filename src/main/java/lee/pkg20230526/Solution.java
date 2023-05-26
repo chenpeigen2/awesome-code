ce97a809f9c585a7d9d2107f04e42b957fa9147e
chenpeigen <chenpeigen@xiaomi.com>
2023-05-26 18:43:03 +0800

    lee-20230526

--- src/main/java/lee/pkg20230526/Solution.java
+++ src/main/java/lee/pkg20230526/Solution.java
@@ -8,27 +8,27 @@ public class Solution {
     //    https://leetcode.cn/problems/shortest-path-in-binary-matrix/
     public int shortestPathBinaryMatrix(int[][] grid) {
         if (grid[0][0] == 1) return -1;
-        int n = grid.length;
 
-        int[][] dist = new int[n][n];
-        for (int i = 0; i < n; i++) {
+        // indicate the dist walking step
+        int len = grid.length;
+        int[][] dist = new int[len][len];
+        for (int i = 0; i < len; i++) {
             Arrays.fill(dist[i], Integer.MAX_VALUE);
         }
+        dist[0][0] = 1;
 
         Queue<int[]> queue = new ArrayDeque<>();
 
         queue.offer(new int[]{0, 0});
-        dist[0][0] = 1;
 
         while (!queue.isEmpty()) {
             int[] arr = queue.poll();
-            int x = arr[0], y = arr[1];
-            if (x == n - 1 && y == n - 1) {
-                return dist[x][y];
-            }
+            int x = arr[0];
+            int y = arr[1];
+            if (x == len - 1 && y == len - 1) return dist[x][y];
             for (int dx = -1; dx <= 1; dx++) {
                 for (int dy = -1; dy <= 1; dy++) {
-                    if (x + dx < 0 || x + dx >= n || y + dy < 0 || y + dy >= n) continue;
+                    if (x + dx < 0 || x + dx >= len || y + dy < 0 || y + dy >= len) continue;
                     // 不可访问 || 已经访问过了
                     if (grid[x + dx][y + dy] == 1 || dist[x + dx][y + dy] <= dist[x][y] + 1) continue;
                     dist[x + dx][y + dy] = dist[x][y] + 1;
