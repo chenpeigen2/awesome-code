1f6be59f8b8ba552ffcc168adbe43e21d684692c
chenpeigen <chenpeigen@xiaomi.com>
2023-05-29 20:23:07 +0800

    lee-20230529

---
+++ src/main/java/lee/pkg20230529/Solution.java
@@ -0,0 +1,22 @@
+package lee.pkg20230529;
+
+public class Solution {
+    //    https://leetcode.cn/problems/average-value-of-even-numbers-that-are-divisible-by-three/
+    public int averageValue(int[] nums) {
+        int sum = 0;
+        int cnt = 0;
+        for (int num : nums) {
+            if (num % 3 == 0 && num % 2 == 0) {
+                sum += num;
+                cnt++;
+            }
+        }
+        if (cnt == 0) return sum;
+        return sum / cnt;
+    }
+
+    public static void main(String[] args) {
+        var app = new Solution();
+        app.averageValue(new int[]{1, 3, 6, 10, 12, 15});
+    }
+}
