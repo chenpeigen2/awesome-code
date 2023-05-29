fc4976ea7fcde037be9c1345b705090745c8b604
chenpeigen <chenpeigen@xiaomi.com>
2023-05-29 20:29:52 +0800

    lee-20230529

--- src/main/java/lee/pkg20230529/Solution.java
+++ src/main/java/lee/pkg20230529/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
         int sum = 0;
         int cnt = 0;
         for (int num : nums) {
-            if (num % 3 == 0 && num % 2 == 0) {
+            if (num % 6 == 0) {
                 sum += num;
                 cnt++;
             }
