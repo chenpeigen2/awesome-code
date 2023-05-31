d8f6ab0d71982ce7b7133106e63003d73ddb099f
chenpeigen <chenpeigen@xiaomi.com>
2023-05-31 22:22:07 +0800

    lee-20230531

--- src/main/java/lee/pkg20230531/Solution.java
+++ src/main/java/lee/pkg20230531/Solution.java
@@ -6,6 +6,7 @@ import java.util.Deque;
 public class Solution {
     //    https://leetcode.cn/problems/minimum-cost-tree-from-leaf-values/
     //    https://leetcode.cn/problems/minimum-cost-tree-from-leaf-values/solution/wei-shi-yao-dan-diao-di-jian-zhan-de-suan-fa-ke-xi/
+    // 通过维护一个单调递减栈就可以找到一个极小值
     public int mctFromLeafValues(int[] arr) {
 
         Deque<Integer> st = new ArrayDeque<>();
