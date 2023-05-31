71f230861ac638d932cb6b8bd3de62158a1b6b32
chenpeigen <chenpeigen@xiaomi.com>
2023-05-31 22:20:38 +0800

    lee-20230531

---
+++ src/main/java/lee/pkg20230531/Solution.java
@@ -0,0 +1,27 @@
+package lee.pkg20230531;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution {
+    //    https://leetcode.cn/problems/minimum-cost-tree-from-leaf-values/
+    //    https://leetcode.cn/problems/minimum-cost-tree-from-leaf-values/solution/wei-shi-yao-dan-diao-di-jian-zhan-de-suan-fa-ke-xi/
+    public int mctFromLeafValues(int[] arr) {
+
+        Deque<Integer> st = new ArrayDeque<>();
+        st.push(Integer.MAX_VALUE);
+        int mct = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            while (arr[i] >= st.peek()) {
+                mct += st.pop() * Math.min(st.peek(), arr[i]);
+            }
+            st.push(arr[i]);
+        }
+        //
+        while (st.size() > 2) {
+            mct += st.pop() * st.peek();
+        }
+        return mct;
+    }
+}
