f45dec3a191afb6485d36d4ed6e3d34252ca5faf
chenpeigen <chenpeigen@xiaomi.com>
2023-06-18 23:20:33 +0800

    lee-20230618

--- src/main/java/pkg20230618/Solution.java
+++ src/main/java/pkg20230618/Solution.java
@@ -17,32 +17,26 @@ public class Solution {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 0) {
                     Queue<int[]> qu = new ArrayDeque<>();
+                    qu.offer(new int[]{i, j});
                     grid[i][j] = 1;
                     boolean closed = true;
-
-                    qu.offer(new int[]{i, j});
-
                     while (!qu.isEmpty()) {
                         int[] arr = qu.poll();
-                        int cx = arr[0], cy = arr[1];
+                        int cx = arr[0];
+                        int cy = arr[1];
                         if (cx == 0 || cy == 0 || cx == m - 1 || cy == n - 1) {
                             closed = false;
                         }
-
-                        // iterator
                         for (int d = 0; d < 4; d++) {
                             int nx = cx + dir[d][0];
                             int ny = cy + dir[d][1];
-                            if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == 0) {
-                                grid[nx][ny] = 1;
-                                qu.offer(new int[]{nx, ny});
-                            }
+                            if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+                            if (grid[nx][ny] == 1) continue;
+                            grid[nx][ny] = 1;
+                            qu.offer(new int[]{nx, ny});
                         }
-
                     }
-
                     if (closed) ans++;
-
                 }
             }
         }
