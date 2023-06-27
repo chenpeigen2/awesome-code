09b003d6fe67f386642b6a2b74cb70446c0340ee
chenpeigen <chenpeigen@xiaomi.com>
2023-06-27 23:31:00 +0800

    lee-20230627

---
+++ src/main/java/lee/pkg20230627/Solution.java
@@ -0,0 +1,18 @@
+package lee.pkg20230627;
+
+public class Solution {
+
+    // https://leetcode.cn/problems/maximum-subarray-sum-with-one-deletion/solution/shan-chu-yi-ci-de-dao-zi-shu-zu-de-zui-d-o1o9/
+    public int maximumSum(int[] arr) {
+        int dp0 = arr[0], dp1 = 0, res = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            dp1 = Math.max(dp0, dp1 + arr[i]);
+            dp0 = Math.max(dp0, 0) + arr[i];
+
+            res = Math.max(res, Math.max(dp0, dp1));
+        }
+
+        return res;
+    }
+}
