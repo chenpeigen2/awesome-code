64275f9dafdf914d4ef801dd934ca1798149aeb8
chenpeigen <chenpeigen@xiaomi.com>
2023-07-10 11:44:16 +0800

    add kt feature

--- kotlin/basic-kotlin/src/main/java/org/peter/lambda/Lambda.kt
+++ kotlin/basic-kotlin/src/main/java/org/peter/lambda/Lambda.kt
@@ -1,10 +1,14 @@
 package org.peter.lambda
 
 fun higherFunc(sum: (x: Int, y: Int) -> Int) {
-    println(sum(1, 2))
+    println(sum(5, 2))
 }
 
 fun main() {
     val sum = { x: Int, y: Int -> x + y }
+    sum.invoke(1, 2)
     higherFunc { x, y -> x + y }
+    higherFunc(fun(x: Int, y: Int): Int {
+        return x * y
+    })
 }
