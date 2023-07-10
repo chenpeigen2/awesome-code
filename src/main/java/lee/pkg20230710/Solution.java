e0a81dc8bd2f4774b4fbc36bdc30ed0a9e01e4f8
chenpeigen <chenpeigen@xiaomi.com>
2023-07-10 22:49:21 +0800

    lee-20230710

---
+++ src/main/java/lee/pkg20230710/Solution.java
@@ -0,0 +1,30 @@
+package lee.pkg20230710;
+
+import java.util.Arrays;
+
+public class Solution {
+    //    https://leetcode.cn/problems/3sum-closest/
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int ans = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length; i++) {
+            int start = i + 1, end = nums.length - 1;
+
+            while (start < end) {
+                int sum = nums[start] + nums[end] + nums[i];
+                if (Math.abs(target - sum) < Math.abs(target - ans)) {
+                    ans = sum;
+                }
+                if (sum > target) {
+                    end--;
+                } else if (sum < target) {
+                    start++;
+                } else {
+                    return ans;
+                }
+            }
+
+        }
+        return ans;
+    }
+}
