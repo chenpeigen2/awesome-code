7d76def8420002612b93c125789ed3cc06845696
chenpeigen <chenpeigen@xiaomi.com>
2023-07-13 23:02:07 +0800

    lee-20230713

--- src/main/java/lee/pkg20230713/Solution.java
+++ src/main/java/lee/pkg20230713/Solution.java
@@ -12,16 +12,13 @@ public class Solution {
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 int mn = dp[i - 1][j];
-
                 if (j > 0) {
                     mn = Math.min(mn, dp[i - 1][j - 1]);
                 }
-                if (j < n - 1) {
+                if (j + 1 < n) {
                     mn = Math.min(mn, dp[i - 1][j + 1]);
                 }
-
                 dp[i][j] = mn + matrix[i][j];
-
             }
         }
         return Arrays.stream(dp[n - 1]).min().getAsInt();
