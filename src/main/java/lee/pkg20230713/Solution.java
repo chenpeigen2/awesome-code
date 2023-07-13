5da3f4e72162ef83b3993d9aaf5ba02c9cbf6499
chenpeigen <chenpeigen@xiaomi.com>
2023-07-13 22:56:54 +0800

    lee-20230713

---
+++ src/main/java/lee/pkg20230713/Solution.java
@@ -0,0 +1,29 @@
+package lee.pkg20230713;
+
+import java.util.Arrays;
+
+public class Solution {
+    //    https://leetcode.cn/problems/minimum-falling-path-sum/
+    public int minFallingPathSum(int[][] matrix) {
+        int n = matrix.length;
+        int[][] dp = new int[n][n];
+        System.arraycopy(matrix[0], 0, dp[0], 0, n);
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int mn = dp[i - 1][j];
+
+                if (j > 0) {
+                    mn = Math.min(mn, dp[i - 1][j - 1]);
+                }
+                if (j < n - 1) {
+                    mn = Math.min(mn, dp[i - 1][j + 1]);
+                }
+
+                dp[i][j] = mn + matrix[i][j];
+
+            }
+        }
+        return Arrays.stream(dp[n - 1]).min().getAsInt();
+    }
+}
