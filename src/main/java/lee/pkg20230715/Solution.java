b604666e39628d1b308b8ea75f413f1fd80bb85c
chenpeigen <chenpeigen@xiaomi.com>
2023-07-15 23:19:49 +0800

    lee-20230715

--- src/main/java/lee/pkg20230715/Solution.java
+++ src/main/java/lee/pkg20230715/Solution.java
@@ -7,47 +7,53 @@ import java.util.List;
 public class Solution {
     //    https://leetcode.cn/problems/4sum/
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> quadruplets = new ArrayList<List<Integer>>();
+        if (nums == null || nums.length < 4) {
+            return quadruplets;
+        }
         Arrays.sort(nums);
-        int n = nums.length;
-
-        List<List<Integer>> ans = new ArrayList<>();
-        if (n < 4) return ans;
-        for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
-            if (nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3] < target) continue;
-            int sum = target - nums[i];
-            for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-                if (nums[j] + nums[j + 1] + nums[j + 2] > sum) break;
-                if (nums[j] + nums[n - 1] + nums[n - 2] < sum) continue;
-
-                int twosum = sum - nums[j];
-                int three = j + 1;
-                int four = n - 1;
-                while (three < four) {
-                    while (three < four && nums[three] + nums[four] < twosum) {
-                        three += 1;
-                    }
-                    while (three < four && nums[three] + nums[four] > twosum) {
-                        four -= 1;
-                    }
-                    if (three != four && nums[three] + nums[four] == twosum) {
-                        List<Integer> list = new ArrayList<>();
-                        list.add(nums[i]);
-                        list.add(nums[j]);
-                        list.add(nums[three]);
-                        list.add(nums[four]);
-                        ans.add(list);
-                        three += 1;
-                        // 如果重复继续three+1
-                        while (three < four && nums[three] == nums[three - 1]) {
-                            three += 1;
+        int length = nums.length;
+        for (int i = 0; i < length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            if ((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                break;
+            }
+            if ((long) nums[i] + nums[length - 3] + nums[length - 2] + nums[length - 1] < target) {
+                continue;
+            }
+            for (int j = i + 1; j < length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                if ((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+                    break;
+                }
+                if ((long) nums[i] + nums[j] + nums[length - 2] + nums[length - 1] < target) {
+                    continue;
+                }
+                int left = j + 1, right = length - 1;
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        quadruplets.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
                         }
+                        left++;
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
                     }
                 }
             }
         }
-        return ans;
+        return quadruplets;
     }
 }
