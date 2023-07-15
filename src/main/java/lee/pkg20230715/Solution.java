75d47b000faa3f60dfdc1d528ea4294922136946
chenpeigen <chenpeigen@xiaomi.com>
2023-07-15 23:08:20 +0800

    lee-20230715

--- src/main/java/lee/pkg20230715/Solution.java
+++ src/main/java/lee/pkg20230715/Solution.java
@@ -11,11 +11,11 @@ public class Solution {
         int n = nums.length;
 
         List<List<Integer>> ans = new ArrayList<>();
-
+        if (n < 4) return ans;
         for (int i = 0; i < n - 3; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
-            if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] < target) continue;
+            if (nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3] < target) continue;
             int sum = target - nums[i];
             for (int j = i + 1; j < n - 2; j++) {
                 if (j > i + 1 && nums[j] == nums[j - 1]) continue;
@@ -26,24 +26,23 @@ public class Solution {
                 int three = j + 1;
                 int four = n - 1;
                 while (three < four) {
-                    int ss = nums[three] + nums[four];
-                    if (ss == twosum) {
+                    while (three < four && nums[three] + nums[four] < twosum) {
+                        three += 1;
+                    }
+                    while (three < four && nums[three] + nums[four] > twosum) {
+                        four -= 1;
+                    }
+                    if (three != four && nums[three] + nums[four] == twosum) {
                         List<Integer> list = new ArrayList<>();
                         list.add(nums[i]);
                         list.add(nums[j]);
                         list.add(nums[three]);
                         list.add(nums[four]);
                         ans.add(list);
-                    }
-                    if (nums[three] + nums[four] < twosum) {
-                        three++;
-                        while (three < four && nums[three - 1] == nums[three]) {
-                            three++;
-                        }
-                    } else {
-                        four--;
-                        while (three < four && nums[four] == nums[four + 1]) {
-                            four--;
+                        three += 1;
+                        // 如果重复继续three+1
+                        while (three < four && nums[three] == nums[three - 1]) {
+                            three += 1;
                         }
                     }
                 }
