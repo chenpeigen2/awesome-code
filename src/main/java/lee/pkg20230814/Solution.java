09f4812d180d381f0171f7ff35e2b470cf457dbb
chenpeigen <chenpeigen@xiaomi.com>
2023-08-14 21:04:56 +0800

    lee-20230814

---
+++ src/main/java/lee/pkg20230814/Solution.java
@@ -0,0 +1,18 @@
+package lee.pkg20230814;
+
+import lee.adds.TreeNode;
+
+public class Solution {
+
+    //    https://leetcode.cn/problems/merge-two-binary-trees/description/
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) return root2;
+        if (root2 == null) return root1;
+
+        TreeNode root = new TreeNode(root1.val + root2.val);
+        root.left = mergeTrees(root1.left, root2.left);
+        root.right = mergeTrees(root1.right, root2.right);
+
+        return root;
+    }
+}
