e9d6520e9036af521f98fcbdd06d7160dc0659de
chenpeigen <chenpeigen@xiaomi.com>
2023-08-16 22:03:17 +0800

    lee-20230816

---
+++ src/main/java/lee/pkg20230816/Solution.java
@@ -0,0 +1,34 @@
+package lee.pkg20230816;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    //    https://leetcode.cn/problems/find-the-losers-of-the-circular-game/
+    public int[] circularGameLosers(int n, int k) {
+        boolean[] visit = new boolean[n];
+        for (int i = k, j = 0; !visit[j]; i += k) {
+            visit[j] = true;
+            j = (j + i) % n;
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                list.add(i + 1);
+            }
+        }
+        int[] ans = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ans[i] = list.get(i);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        var app = new Solution();
+        var ans = app.circularGameLosers(5, 2);
+        System.out.println(ans);
+    }
+}
