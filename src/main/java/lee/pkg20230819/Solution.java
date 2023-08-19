e59efad1409df0f37d92e4027df7d14b8d0cac64
chenpeigen <chenpeigen@xiaomi.com>
2023-08-19 22:06:02 +0800

    lee-20230819

---
+++ src/main/java/lee/pkg20230819/Solution.java
@@ -0,0 +1,8 @@
+package lee.pkg20230819;
+
+public class Solution {
+    //    https://leetcode.cn/problems/add-two-integers/
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
