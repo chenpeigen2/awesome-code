53f4fab51b4ef25d20e742a9a10f66760750d63a
chenpeigen <chenpeigen@xiaomi.com>
2023-08-19 19:55:48 +0800

    lee-add

--- src/main/java/lee/adds/Solution.java
+++ src/main/java/lee/adds/Solution.java
@@ -540,4 +540,17 @@ public class Solution {
         }
         return right;
     }
+
+    //    https://leetcode.cn/problems/intersection-of-two-linked-lists/
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+
 }
