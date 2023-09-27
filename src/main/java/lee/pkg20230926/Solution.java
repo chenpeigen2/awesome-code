042cf434ee3e3068a1176025cfdcc75f4140852d
chenpeigen <chenpeigen@xiaomi.com>
2023-09-27 21:22:51 +0800

    lee-20230926

--- src/main/java/lee/pkg20230926/Solution.java
+++ src/main/java/lee/pkg20230926/Solution.java
@@ -1,6 +1,7 @@
 package lee.pkg20230926;
 
 public class Solution {
+    //    https://leetcode.cn/problems/pass-the-pillow/description/?envType=daily-question&envId=2023-09-26
     public int passThePillow(int n, int time) {
         int left = time % (n - 1);
         int count = time / (n - 1);
