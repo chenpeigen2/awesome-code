1d489eb64273a2a5c0c73abc8ca7a0eabb6ea135
chenpeigen <chenpeigen@xiaomi.com>
2023-11-02 22:36:49 +0800

    lee-hot100

---
+++ src/main/java/hot100/Solution.java
@@ -0,0 +1,35 @@
+package hot100;
+
+import java.util.*;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String s = new String(arr);
+            List<String> list = map.getOrDefault(s, new ArrayList<>());
+            list.add(str);
+            map.put(s, list);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public int longestConsecutive(int[] nums) {
+        return -1;
+    }
+}
