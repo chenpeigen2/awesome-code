1f3c7f9c638316a9d68b8e542cff753f0e1bfb1e
chenpeigen <chenpeigen@xiaomi.com>
2023-11-04 20:33:48 +0800

    lee-20231104

--- src/main/java/lee/pkg20231104/Solution.java
+++ src/main/java/lee/pkg20231104/Solution.java
@@ -6,24 +6,23 @@ import java.util.Set;
 public class Solution {
     //    https://leetcode.cn/problems/maximum-xor-of-two-numbers-in-an-array/?envType=daily-question&envId=2023-11-04
     public int findMaximumXOR(int[] nums) {
-        int res = 0;
+        int ret = 0;
         int mask = 0;
         for (int i = 30; i >= 0; i--) {
             mask = mask | (1 << i);
-
             Set<Integer> set = new HashSet<>();
             for (int num : nums) {
                 set.add(num & mask);
             }
-
-            int temp = res | (1 << i);
+            int temp = ret | (1 << i);
             for (Integer prefix : set) {
-                if (set.contains(prefix ^ temp)) {
-                    res = temp;
+                if (set.contains(temp ^ prefix)) {
+                    ret = temp;
                     break;
                 }
             }
         }
-        return res;
+
+        return ret;
     }
 }
