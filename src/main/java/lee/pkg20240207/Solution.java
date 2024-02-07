e5b59782f9b6a12c4ec6f017813e31f6e35cb234
chenpeigen <chenpeigen@xiaomi.com>
2024-02-07 11:00:58 +0800

    lee-20240207

---
+++ src/main/java/lee/pkg20240207/Solution.java
@@ -0,0 +1,47 @@
+package lee.pkg20240207;
+
+import lee.adds.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Solution {
+    //    https://leetcode.cn/problems/cousins-in-binary-tree-ii/?envType=daily-question&envId=2024-02-07
+    public TreeNode replaceValueInTree(TreeNode root) {
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        root.val = 0;
+        while (!queue.isEmpty()) {
+            Queue<TreeNode> queue2 = new ArrayDeque<TreeNode>();
+            int sum = 0;
+
+            for (TreeNode fa : queue) {
+                if (fa.left != null) {
+                    queue2.offer(fa.left);
+                    sum += fa.left.val;
+                }
+                if (fa.right != null) {
+                    queue2.offer(fa.right);
+                    sum += fa.right.val;
+                }
+            }
+
+            for (TreeNode fa : queue) {
+                int childSum = (fa.left != null ? fa.left.val : 0) +
+                        (fa.right != null ? fa.right.val : 0);
+                if (fa.left != null) {
+                    fa.left.val = sum - childSum;
+                }
+                if (fa.right != null) {
+                    fa.right.val = sum - childSum;
+                }
+            }
+
+            queue = queue2;
+
+
+        }
+
+        return root;
+    }
+}
