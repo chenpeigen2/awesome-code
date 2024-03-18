c8c941ac55bb3e9b1aff23ac7ce52e685e04f5e8
chenpeigen <chenpeigen@xiaomi.com>
2024-03-18 23:45:52 +0800

    lee-20240318

---
+++ src/main/java/lee/pkg20240318/Solution.java
@@ -0,0 +1,23 @@
+package lee.pkg20240318;
+
+class Solution {
+}
+
+
+class NumArray {
+
+    private int[] nums;
+
+    public NumArray(int[] nums) {
+        this.nums = nums;
+    }
+
+    public int sumRange(int left, int right) {
+        int sum = 0;
+        for (int i = left; i <= right; i++) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+
+}
