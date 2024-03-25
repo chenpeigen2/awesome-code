8988358c09ef1a34bf534dbb45f82ae32aab1f6f
chenpeigen <chenpeigen@xiaomi.com>
2024-03-25 23:24:24 +0800

    lee-20240324

---
+++ src/main/java/lee/pkg20240325/Solution.java
@@ -0,0 +1,26 @@
+package lee.pkg20240325;
+
+import java.util.Arrays;
+
+public class Solution {
+    //    https://leetcode.cn/problems/coin-change/?envType=daily-question&envId=2024-03-24
+    public int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+
+        Arrays.fill(dp, max);
+
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+
+    }
+}
