059219ee10a7f9287c4ad7acf9d712dcdcf6c4b7
chenpeigen <chenpeigen@xiaomi.com>
2024-03-26 19:46:11 +0800

    lee-20240326

---
+++ src/main/java/lee/pkg20240326/Solution.java
@@ -0,0 +1,20 @@
+package lee.pkg20240326;
+
+public class Solution {
+}
+
+//https://leetcode.cn/problems/design-graph-with-shortest-path-calculator/?envType=daily-question&envId=2024-03-26
+class Graph {
+
+    public Graph(int n, int[][] edges) {
+
+    }
+
+    public void addEdge(int[] edge) {
+
+    }
+
+    public int shortestPath(int node1, int node2) {
+
+    }
+}
