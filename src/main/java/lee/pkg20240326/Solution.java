25c3b49514a3e06501d56a86d3858af46b598c91
chenpeigen <chenpeigen@xiaomi.com>
2024-03-26 20:19:27 +0800

    lee-20240326

--- src/main/java/lee/pkg20240326/Solution.java
+++ src/main/java/lee/pkg20240326/Solution.java
@@ -1,20 +1,52 @@
 package lee.pkg20240326;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class Solution {
 }
 
 //https://leetcode.cn/problems/design-graph-with-shortest-path-calculator/?envType=daily-question&envId=2024-03-26
 class Graph {
 
-    public Graph(int n, int[][] edges) {
 
+    private List<int[]>[] graph;
+
+
+    public Graph(int n, int[][] edges) {
+        graph = new List[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int[] edge : edges) {
+            graph[edge[0]].add(new int[]{edge[1], edge[2]});
+        }
     }
 
     public void addEdge(int[] edge) {
-
+        graph[edge[0]].add(new int[]{edge[1], edge[2]});
     }
 
     public int shortestPath(int node1, int node2) {
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> a[0] - b[0]);
+        int[] dist = new int[graph.length];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[node1] = 0;
+        pq.offer(new int[]{0, node1});
+        while (!pq.isEmpty()) {
+            int[] arr = pq.poll();
+            int cost = arr[0], cur = arr[1];
+            if (cur == node2) return cost;
+            for (int[] nextArr : graph[cur]) {
+                int next = nextArr[0], ncost = nextArr[1];
+                if (dist[next] > cost + ncost) {
+                    pq.offer(new int[]{cost + ncost, next});
+                }
+            }
+        }
 
+        return -1;
     }
 }
