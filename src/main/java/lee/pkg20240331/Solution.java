b4fafc68d3055e8bc6d233f16d73f4d0f7b7bddc
chenpeigen <chenpeigen@xiaomi.com>
2024-03-31 21:35:44 +0800

    lee-20240331

---
+++ src/main/java/lee/pkg20240331/Solution.java
@@ -0,0 +1,45 @@
+package lee.pkg20240331;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution {
+    public boolean isValidSerialization(String preorder) {
+        int n = preorder.length();
+        int i = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(1);
+
+        while (i < n) {
+            if (stack.isEmpty()) return false;
+            if (preorder.charAt(i) == ',') {
+                i++;
+            } else if (preorder.charAt(i) == '#') {
+                int top = stack.pop() - 1;
+
+                if (top > 0) {
+                    stack.push(top);
+                }
+
+                i++;
+            } else {
+                while (i < n && preorder.charAt(i) != ',') {
+                    i++;
+                }
+
+                int top = stack.pop() - 1;
+
+                if (top > 0) {
+                    stack.push(top);
+                }
+
+
+                stack.push(2);
+            }
+        }
+
+
+        return stack.isEmpty();
+
+    }
+}
