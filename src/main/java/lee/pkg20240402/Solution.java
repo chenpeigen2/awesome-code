b6d9199901492f662f23433b732b1a2c85aae191
chenpeigen <chenpeigen@xiaomi.com>
2024-04-02 20:47:33 +0800

    lee-20240402

--- src/main/java/lee/pkg20240402/Solution.java
+++ src/main/java/lee/pkg20240402/Solution.java
@@ -50,30 +50,26 @@ public class Solution {
     }
 
     public List<TreeNode> allPossibleFBT1(int n) {
-        if (n % 2 == 0) {
-            return new ArrayList<>();
-        }
+        if (n % 2 == 0) return new ArrayList<>();
 
         List<TreeNode>[] dp = new List[n + 1];
+
         for (int i = 0; i <= n; i++) {
             dp[i] = new ArrayList<>();
         }
 
         dp[1].add(new TreeNode(0));
 
-        for (int i = 3; i <= n; i += 2) {
+        for (int i = 3; i <= n; i++) {
             for (int j = 1; j < i; j += 2) {
                 for (TreeNode leftSubtree : dp[j]) {
-                    for (TreeNode rightSubtrees : dp[i - j - 1]) {
-                        TreeNode root = new TreeNode(0, leftSubtree, rightSubtrees);
-                        dp[i].add(root);
+                    for (TreeNode rightSubtree : dp[i - 1 - j]) {
+                        dp[i].add(new TreeNode(0, leftSubtree, rightSubtree));
                     }
                 }
             }
         }
-
         return dp[n];
-
     }
 
 
