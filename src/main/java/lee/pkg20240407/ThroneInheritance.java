d16c4e82e40ea79541caf86d62f2e162e173362d
chenpeigen <chenpeigen@xiaomi.com>
2024-04-07 20:06:45 +0800

    lee-20240407

---
+++ src/main/java/lee/pkg20240407/ThroneInheritance.java
@@ -0,0 +1,41 @@
+package lee.pkg20240407;
+
+import java.util.*;
+
+// https://leetcode.cn/problems/throne-inheritance/solutions/2726886/python3javacgotypescript-yi-ti-yi-jie-du-bm16/?envType=daily-question&envId=2024-04-07
+public class ThroneInheritance {
+
+    private String king;
+    private Set<String> dead = new HashSet<>();
+    private Map<String, List<String>> g = new HashMap<>();
+
+    private List<String> ans = new ArrayList<>();
+
+
+    public ThroneInheritance(String kingName) {
+        kingName = kingName;
+    }
+
+    public void birth(String parentName, String childName) {
+        g.computeIfAbsent(parentName, k -> new ArrayList<>()).add(childName);
+    }
+
+    public void death(String name) {
+        dead.add(name);
+    }
+
+    public List<String> getInheritanceOrder() {
+        ans.clear();
+        dfs(king);
+        return ans;
+    }
+
+    private void dfs(String x) {
+        if (!dead.contains(x)) {
+            ans.add(x);
+        }
+        for (String y : g.getOrDefault(x, List.of())) {
+            dfs(y);
+        }
+    }
+}
