782d366f9e04498244c27cbc1429c907e9fa9c3c
chenpeigen <chenpeigen@xiaomi.com>
2024-04-13 16:59:11 +0800

    lee-add

--- src/main/java/lee/pkg20240410/Solution.java
+++ src/main/java/lee/pkg20240410/Solution.java
@@ -1,6 +1,8 @@
 package lee.pkg20240410;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Solution {
     //    https://leetcode.cn/problems/maximum-binary-string-after-change/description/?envType=daily-question&envId=2024-04-10
@@ -86,6 +88,15 @@ public class Solution {
             }
         }
         return s;
+    }
+
+    //    https://leetcode.cn/problems/reverse-words-in-a-string/?envType=study-plan-v2&envId=top-interview-150
+    public String reverseWords(String s) {
+        s = s.strip();
+        // 正则匹配连续的空白字符作为分隔符分割
+        List<String> wordList = Arrays.asList(s.split("\\s+"));
+        Collections.reverse(wordList);
+        return String.join(" ", wordList);
     }
 
 }
