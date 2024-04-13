4a3c04216f5f627057c948af3384fee453239462
chenpeigen <chenpeigen@xiaomi.com>
2024-04-13 17:28:46 +0800

    lee-add

--- src/main/java/lee/pkg20240410/Solution.java
+++ src/main/java/lee/pkg20240410/Solution.java
@@ -121,4 +121,26 @@ public class Solution {
         return haystack.indexOf(needle);
     }
 
+    //    https://leetcode.cn/problems/valid-palindrome/?envType=study-plan-v2&envId=top-interview-150
+    public boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+        while (l < r) {
+            while (l < r && !Character.isLetterOrDigit(s.charAt(l))) {
+                ++l;
+            }
+            while (l < r && !Character.isLetterOrDigit(s.charAt(r))) {
+                --r;
+            }
+            if (l < r) {
+                if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                    return false;
+                }
+                ++l;
+                --r;
+            }
+        }
+        return true;
+    }
+
 }
