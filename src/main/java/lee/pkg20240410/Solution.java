49d44209ad4c1b67900d3a4174ce0c3a53b2d94a
chenpeigen <chenpeigen@xiaomi.com>
2024-04-14 20:59:46 +0800

    lee-add

--- src/main/java/lee/pkg20240410/Solution.java
+++ src/main/java/lee/pkg20240410/Solution.java
@@ -229,4 +229,22 @@ public class Solution {
         return area;
     }
 
+    //    https://leetcode.cn/problems/minimum-size-subarray-sum/?envType=study-plan-v2&envId=top-interview-150
+    public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+
+        int ans = Integer.MAX_VALUE;
+        int start = 0, end = 0;
+        int sum = 0;
+        while (end < n) {
+            sum += nums[end++];
+            while (sum >= target) {
+                ans = Math.min(ans, end - start);
+                sum -= nums[start++];
+            }
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+
 }
