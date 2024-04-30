ee466c984f70df25428d3b1d4b91466991b21331
chenpeigen <chenpeigen@xiaomi.com>
2024-04-30 21:57:33 +0800

    lee-add

--- src/main/java/hot100/Solution.java
+++ src/main/java/hot100/Solution.java
@@ -546,6 +546,28 @@ public class Solution {
     }
 
 
+    HashMap<Integer, Integer> memo = new HashMap<>();
+    int[] post;
+
+    //    https://leetcode.cn/problems/construct-binary-tree-from-inorder-and-postorder-traversal/?envType=study-plan-v2&envId=top-interview-150
+    public TreeNode buildTree1(int[] inorder, int[] postorder) {
+        for (int i = 0; i < inorder.length; i++) memo.put(inorder[i], i);
+        post = postorder;
+        return buildTree(0, inorder.length - 1, 0, post.length - 1);
+    }
+
+    public TreeNode buildTree(int is, int ie, int ps, int pe) {
+        if (ie < is || pe < ps) return null;
+        int root = post[pe];
+        int ri = memo.get(root);
+
+        TreeNode node = new TreeNode(root);
+        node.left = buildTree(is, ri - 1, ps, ps + ri - is - 1);
+        node.right = buildTree(ri + 1, ie, ps + ri - is, pe - 1);
+        return node;
+    }
+
+
     public static void main(String[] args) {
         var app = new Solution();
         app.removeDuplicates1(new int[]{2, 2, 2, 3});
