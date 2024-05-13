b1cb233a4d274bd172d4611ef1d4933a351fe00a
chenpeigen <chenpeigen@xiaomi.com>
2024-05-13 10:57:34 +0800

    upgrade kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("java")
-    kotlin("jvm") version "1.9.23"
-    kotlin("kapt") version "1.9.23"
+    kotlin("jvm") version "1.9.24"
+    kotlin("kapt") version "1.9.24"
 }
 
 group = "org.peter"
