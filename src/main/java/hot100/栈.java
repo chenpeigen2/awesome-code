8f8b07f7e54c94cb32a621989e2183f168dbefe8
chenpeigen <chenpeigen@xiaomi.com>
2024-05-30 19:39:20 +0800

    lee-add

--- src/main/java/hot100/栈.java
+++ src/main/java/hot100/栈.java
@@ -2,6 +2,8 @@ package hot100;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 public class 栈 {
     //    https://leetcode.cn/problems/valid-parentheses/?envType=study-plan-v2&envId=top-100-liked
@@ -20,4 +22,41 @@ public class 栈 {
         }
         return stack.isEmpty();
     }
+
+    //    https://leetcode.cn/problems/min-stack/?envType=study-plan-v2&envId=top-100-liked
+    class MinStack {
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        Deque<Integer> minStack;
+
+        public MinStack() {
+            minStack = new LinkedList<Integer>();
+            minStack.push(Integer.MAX_VALUE);
+        }
+
+        public void push(int val) {
+            stack.push(val);
+            queue.offer(val);
+            // 维护一个单调最小的数值
+            minStack.push(Math.min(minStack.peek(), val));
+        }
+
+        public void pop() {
+            int value = stack.pop();
+            queue.remove(value);
+            minStack.pop();
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public int getMin() {
+//            return minStack.peek()
+            return queue.peek();
+        }
+    }
+
 }
