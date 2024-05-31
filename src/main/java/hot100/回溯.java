f8046c2288be7302b4aa0cd2ebcb3a27923f7c15
chenpeigen <chenpeigen@xiaomi.com>
2024-05-31 21:36:48 +0800

    lee-add

--- src/main/java/hot100/回溯.java
+++ src/main/java/hot100/回溯.java
@@ -1,8 +1,6 @@
 package hot100;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class 回溯 {
     // O(n×n!)
@@ -59,7 +57,28 @@ public class 回溯 {
         }
     }
 
+    //    https://leetcode.cn/problems/combination-sum/solutions/?envType=study-plan-v2&envId=top-100-liked
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        int len = candidates.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        if (len == 0) return ans;
+        Deque<Integer> path = new ArrayDeque<>();
+        dfs(candidates, 0, len, target, path, ans);
+        return ans;
+    }
 
-
+    private void dfs(int[] candidates, int begin, int len, int target,
+                     Deque<Integer> path, List<List<Integer>> res) {
+        if (target < 0) return;
+        if (target == 0) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = begin; i < len; i++) {
+            path.push(candidates[i]);
+            dfs(candidates, i, len, target - candidates[i], path, res);
+            path.pop();
+        }
+    }
 
 }
