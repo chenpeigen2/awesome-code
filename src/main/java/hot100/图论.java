32df7952b48aca7fe981d3177ee8ac92da330bdd
chenpeigen <chenpeigen@xiaomi.com>
2024-06-21 16:09:53 +0800

    lee-add

--- src/main/java/hot100/图论.java
+++ src/main/java/hot100/图论.java
@@ -34,4 +34,58 @@ public class 图论 {
             }
         }
     }
+
+    //    https://leetcode.cn/problems/rotting-oranges/?envType=study-plan-v2&envId=top-100-liked
+    public int orangesRotting(int[][] grid) {
+        int M = grid.length;
+        int N = grid[0].length;
+        Queue<int[]> queue = new ArrayDeque<>();
+
+        int count = 0;
+        for (int r = 0; r < M; r++) {
+            for (int c = 0; c < N; c++) {
+                if (grid[r][c] == 1) {
+                    count++;
+                } else if (grid[r][c] == 2) {
+                    queue.offer(new int[]{r, c});
+                }
+            }
+        }
+
+        int round = 0;
+        while (count > 0 && !queue.isEmpty()) {
+            round++;
+            int n = queue.size();
+
+            for (int i = 0; i < n; i++) {
+                int[] orange = queue.poll();
+                int r = orange[0];
+                int c = orange[1];
+
+                if (r - 1 >= 0 && grid[r - 1][c] == 1) {
+                    grid[r - 1][c] = 2;
+                    count--;
+                    queue.offer(new int[]{r - 1, c});
+                }
+
+                if (r + 1 < M && grid[r + 1][c] == 1) {
+                    grid[r + 1][c] = 2;
+                    count--;
+                    queue.add(new int[]{r + 1, c});
+                }
+                if (c - 1 >= 0 && grid[r][c - 1] == 1) {
+                    grid[r][c - 1] = 2;
+                    count--;
+                    queue.add(new int[]{r, c - 1});
+                }
+                if (c + 1 < N && grid[r][c + 1] == 1) {
+                    grid[r][c + 1] = 2;
+                    count--;
+                    queue.add(new int[]{r, c + 1});
+                }
+            }
+        }
+        if (count > 0) return -1;
+        return round;
+    }
 }
