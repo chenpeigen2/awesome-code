80c4dcf2c6f5e89ac21f7851f2d3587ebcc3c06f
chenpeigen <chenpeigen@xiaomi.com>
2024-06-26 19:09:20 +0800

    lee add

--- src/main/java/hot100/栈.java
+++ src/main/java/hot100/栈.java
@@ -53,4 +53,30 @@ public class 栈 {
         }
     }
 
+    //    https://leetcode.cn/problems/decode-string/?envType=study-plan-v2&envId=top-100-liked
+    public String decodeString(String s) {
+        StringBuilder res = new StringBuilder();
+        int multi = 0;
+        Deque<Integer> stack_multi = new LinkedList<>();
+        Deque<String> stack_res = new LinkedList<>();
+        for (Character c : s.toCharArray()) {
+            if (c == '[') {
+                stack_multi.push(multi);
+                stack_res.push(res.toString());
+                multi = 0;
+                res = new StringBuilder();
+            } else if (c == ']') {
+                StringBuilder tmp = new StringBuilder();
+                int cur_multi = stack_multi.pop();
+                for (int i = 0; i < cur_multi; i++) tmp.append(res);
+                res = new StringBuilder(stack_res.pop() + tmp);
+            } else if (c >= '0' && c <= '9') {
+                multi = multi * 10 + Integer.parseInt(c + "");
+            } else {
+                res.append(c);
+            }
+        }
+        return res.toString();
+    }
+
 }
