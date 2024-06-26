f2cc7b4b741cffe074b00a07099908d80584dfca
chenpeigen <chenpeigen@xiaomi.com>
2024-06-26 15:41:08 +0800

    lee-add

--- src/main/java/hot100/贪心.java
+++ src/main/java/hot100/贪心.java
@@ -4,12 +4,11 @@ public class 贪心 {
     //    https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/submissions/518165430/?envType=study-plan-v2&envId=top-100-liked
     public int maxProfit(int[] prices) {
         int min = prices[0];
-
-        int res = 0;
+        int ans = 0;
         for (int price : prices) {
-            res = Math.max(res, price - min);
+            ans = Math.max(ans, price - min);
             min = Math.min(min, price);
         }
-        return res;
+        return ans;
     }
 }
