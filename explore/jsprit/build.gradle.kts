c8494473f51ee32baced2dd50113c273eb6ef4b6
chenpeigen <chenpeigen@ruc.edu.cn>
2025-02-17 11:02:35 +0800

    add jsprit

---
+++ explore/jsprit/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "org.peter"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+// https://mvnrepository.com/artifact/com.graphhopper/jsprit-core
+    implementation("com.graphhopper:jsprit-core:1.9.0-beta.12")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
