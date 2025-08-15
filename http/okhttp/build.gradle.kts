cf14b03c6de368b98fc70aa724d4e4ae525fc81f
chenpeigen <chenpeigen@baidu.com>
2025-08-15 11:42:39 +0800

    upgrade depen

--- http/okhttp/build.gradle.kts
+++ http/okhttp/build.gradle.kts
@@ -13,9 +13,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     // https://mvnrepository.com/artifact/com.squareup.okhttp3/okhttp
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:okhttp:5.1.0")
     // https://mvnrepository.com/artifact/com.google.guava/guava
-    implementation("com.google.guava:guava:33.4.6-jre")
+    implementation("com.google.guava:guava:33.4.8-jre")
 }
 
 tasks.test {
