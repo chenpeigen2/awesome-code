c8be9a48ca386093bf5ca849b009b3275c8da4a5
chenpeigen <chenpeigen@baidu.com>
2025-08-28 21:42:01 +0800

    androidx

--- android/androidx/app/build.gradle.kts
+++ android/androidx/app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation(fileTree(mapOf("dir" to "../libs", "include" to listOf("*.aar"))))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
