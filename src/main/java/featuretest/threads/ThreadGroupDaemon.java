48ec63cd21e19c9022d57a83f4469da5ea634607
chenpeigen <chenpeigen@baidu.com>
2025-08-28 20:21:39 +0800

    fix broken

--- src/main/java/featuretest/threads/ThreadGroupDaemon.java
+++ src/main/java/featuretest/threads/ThreadGroupDaemon.java
@@ -6,25 +6,27 @@ public class ThreadGroupDaemon {
 
     public static void main(String[] args) throws InterruptedException {
         ThreadGroup group1 = new ThreadGroup("group1");
-        new Thread(group1, () -> {
+        Thread thread1 = new Thread(group1, () -> {
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }, "group1-thread1").start();
-        ThreadGroup group2 = new ThreadGroup("group2");
-        new Thread(group2, () -> {
-            try {
-                TimeUnit.SECONDS.sleep(1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }, "group1-thread2").start();
-        group2.setDaemon(true);
+        }, "group1-thread1");
+        thread1.start();
 
-        TimeUnit.SECONDS.sleep(3);
-        System.out.println(group1.isDestroyed());
-        System.out.println(group2.isDestroyed());
+        ThreadGroup group2 = new ThreadGroup("group2");
+        Thread thread2 = new Thread(group2, () -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "group2-thread2");
+        thread2.start();
+
+        // 等待所有线程结束
+        thread1.join();
+        thread2.join();
     }
 }
