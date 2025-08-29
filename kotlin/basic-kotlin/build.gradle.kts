11b38f0b88247a6585c9c650a788534d4b0a0b7a
chenpeigen <chenpeigen@baidu.com>
2025-08-29 13:25:13 +0800

    depend add

--- kotlin/basic-kotlin/build.gradle.kts
+++ kotlin/basic-kotlin/build.gradle.kts
@@ -12,7 +12,17 @@ repositories {
 
 dependencies {
     // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-datetime-jvm
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime-jvm:0.7.1")
+    // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-metadata-jvm
+    implementation("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.9.0")
+    // https://mvnrepository.com/artifact/com.google.code.gson/gson
+    implementation("com.google.code.gson:gson:2.13.1")
+    // https://mvnrepository.com/artifact/com.squareup.okhttp3/okhttp
+    implementation("com.squareup.okhttp3:okhttp:5.1.0")
+    // https://mvnrepository.com/artifact/com.squareup.okio/okio
+    implementation("com.squareup.okio:okio:3.16.0")
     testImplementation(platform("org.junit:junit-bom:5.11.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
