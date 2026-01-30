8a876e343ba1cf2839a97efe5b32c7d9215481dd
chenpeigen <chenpeigen@xiaomi.com>
2026-01-30 14:26:23 +0800

    add 代码-zig

---
+++ zig/hello-world.zig
@@ -0,0 +1,21 @@
+const mem = @import("std").mem; // will be used to compare bytes
+const print = @import("std").debug.print;
+
+pub fn main() !void {
+    try @import("std").fs.File.stdout().writeAll("Hello world");
+    const bytes = "hello";
+    print("{}\n", .{@TypeOf(bytes)}); // *const [5:0]u8
+    print("{d}\n", .{bytes.len}); // 5
+    print("{c}\n", .{bytes[1]}); // 'e'
+    print("{d}\n", .{bytes[5]}); // 0
+    print("{}\n", .{'e' == '\x65'}); // true
+    print("{d}\n", .{'\u{1f4a9}'}); // 128169
+    print("{d}\n", .{'💯'}); // 128175
+    print("{u}\n", .{'⚡'});
+    print("{}\n", .{mem.eql(u8, "hello", "h\x65llo")}); // true
+    print("{}\n", .{mem.eql(u8, "💯", "\xf0\x9f\x92\xaf")}); // also true
+    const invalid_utf8 = "\xff\xfe"; // non-UTF-8 strings are possible with \xNN notation.
+    print("0x{x}\n", .{invalid_utf8[1]}); // indexing them returns individual bytes...
+    print("0x{x}\n", .{"💯"[1]}); // ...as does indexing part-way through non-ASCII characters
+
+}
