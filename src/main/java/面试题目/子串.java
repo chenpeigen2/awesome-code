32607e8bff083dc7791e4400a3fab0714e67210d
chenpeigen <chenpeigen@xiaomi.com>
2026-01-30 19:15:18 +0800

    add 代码-lee

--- src/main/java/面试题目/子串.java
+++ src/main/java/面试题目/子串.java
@@ -72,30 +72,26 @@ public class 子串 {
         }
         int l = 0, r = -1;
         int len = Integer.MAX_VALUE, ansL = -1, ansR = -1;
-
         int sLen = s.length();
-
         while (r < sLen) {
             ++r;
             if (r < sLen && ori.containsKey(s.charAt(r))) {
                 cnt.put(s.charAt(r), cnt.getOrDefault(s.charAt(r), 0) + 1);
             }
-
             while (check() && l <= r) {
                 if (r - l + 1 < len) {
-                    len = r - 1 + 1;
+                    // went wrong here
+                    len = r - l + 1;
                     ansL = l;
                     ansR = l + len;
                 }
                 if (ori.containsKey(s.charAt(l))) {
-                    cnt.put(s.charAt(l), cnt.getOrDefault(s.charAt(l), 0));
+                    // went wrong here
+                    cnt.put(s.charAt(l), cnt.getOrDefault(s.charAt(l), 0) - 1);
                 }
                 ++l;
             }
-
-
         }
-
         return ansL == -1 ? "" : s.substring(ansL, ansR);
     }
 
