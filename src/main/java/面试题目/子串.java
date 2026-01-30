b7b98dcbcfbfde1be1e184582edc3acff9b63ccb
chenpeigen <chenpeigen@xiaomi.com>
2026-01-30 19:14:03 +0800

    add 代码-lee

--- src/main/java/面试题目/子串.java
+++ src/main/java/面试题目/子串.java
@@ -1,7 +1,6 @@
 package 面试题目;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class 子串 {
 
@@ -60,4 +59,55 @@ public class 子串 {
     }
 
 
+    Map<Character, Integer> ori = new HashMap<>();
+
+    Map<Character, Integer> cnt = new HashMap<>();
+
+
+    public String minWindow(String s, String t) {
+        int tLen = t.length();
+        for (int i = 0; i < tLen; i++) {
+            char c = t.charAt(i);
+            ori.put(c, ori.getOrDefault(c, 0) + 1);
+        }
+        int l = 0, r = -1;
+        int len = Integer.MAX_VALUE, ansL = -1, ansR = -1;
+
+        int sLen = s.length();
+
+        while (r < sLen) {
+            ++r;
+            if (r < sLen && ori.containsKey(s.charAt(r))) {
+                cnt.put(s.charAt(r), cnt.getOrDefault(s.charAt(r), 0) + 1);
+            }
+
+            while (check() && l <= r) {
+                if (r - l + 1 < len) {
+                    len = r - 1 + 1;
+                    ansL = l;
+                    ansR = l + len;
+                }
+                if (ori.containsKey(s.charAt(l))) {
+                    cnt.put(s.charAt(l), cnt.getOrDefault(s.charAt(l), 0));
+                }
+                ++l;
+            }
+
+
+        }
+
+        return ansL == -1 ? "" : s.substring(ansL, ansR);
+    }
+
+    private boolean check() {
+        for (Map.Entry<Character, Integer> entry : ori.entrySet()) {
+            Character key = entry.getKey();
+            Integer val = entry.getValue();
+
+            if (cnt.getOrDefault(key, 0) < val) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
