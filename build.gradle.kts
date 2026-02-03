7861aa2e95947401c88ddf637e3ea86925b54491
chenpeigen <chenpeigen@xiaomi.com>
2026-02-03 21:45:00 +0800

    add 代码-lee

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,79 +10,6 @@ plugins {
 group = "org.peter"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    // 阿里云镜像
-    maven {
-        url = uri("https://maven.aliyun.com/repository/public")
-    }
-    maven {
-        url = uri("https://maven.aliyun.com/repository/central")
-    }
-    maven {
-        url = uri("https://maven.aliyun.com/repository/gradle-plugin")
-    }
-    // 腾讯云镜像
-    maven {
-        url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
-    }
-    // 官方仓库作为备用
-    mavenCentral()
-    gradlePluginPortal()
-}
-
-dependencies {
-    // https://mvnrepository.com/artifact/org.openjdk.nashorn/nashorn-core
-    implementation("org.openjdk.nashorn:nashorn-core:15.4")
-// https://mvnrepository.com/artifact/commons-codec/commons-codec
-    implementation("commons-codec:commons-codec:1.20.0")
-// https://mvnrepository.com/artifact/org.bouncycastle/bcprov-jdk15on
-    implementation("org.bouncycastle:bcprov-jdk15on:1.70")
-    testImplementation(platform("org.junit:junit-bom:5.14.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    // https://mvnrepository.com/artifact/com.google.guava/guava
-    implementation("com.google.guava:guava:33.5.0-jre")
-}
-
-group = "org.peter"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    // 阿里云镜像
-    maven {
-        url = uri("https://maven.aliyun.com/repository/public")
-    }
-    maven {
-        url = uri("https://maven.aliyun.com/repository/central")
-    }
-    maven {
-        url = uri("https://maven.aliyun.com/repository/gradle-plugin")
-    }
-    // 腾讯云镜像
-    maven {
-        url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
-    }
-    // 官方仓库作为备用
-    mavenCentral()
-    gradlePluginPortal()
-}
-
-dependencies {
-    // https://mvnrepository.com/artifact/org.openjdk.nashorn/nashorn-core
-    implementation("org.openjdk.nashorn:nashorn-core:15.4")
-// https://mvnrepository.com/artifact/commons-codec/commons-codec
-    implementation("commons-codec:commons-codec:1.20.0")
-// https://mvnrepository.com/artifact/org.bouncycastle/bcprov-jdk15on
-    implementation("org.bouncycastle:bcprov-jdk15on:1.70")
-    testImplementation(platform("org.junit:junit-bom:5.14.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    // https://mvnrepository.com/artifact/com.google.guava/guava
-    implementation("com.google.guava:guava:33.5.0-jre")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
 allprojects {
     repositories {
         // 阿里云镜像
@@ -103,7 +30,7 @@ allprojects {
         mavenCentral()
         gradlePluginPortal()
     }
-    
+
     tasks.withType<JavaCompile> {
         options.release.set(25)
         options.isIncremental = true
@@ -115,4 +42,24 @@ allprojects {
         compilerOptions.freeCompilerArgs.set(listOf("-Xjsr305=strict"))
         compilerOptions.jvmTarget.set(JvmTarget.JVM_25)
     }
+}
+
+dependencies {
+    // https://mvnrepository.com/artifact/org.openjdk.nashorn/nashorn-core
+    implementation("org.openjdk.nashorn:nashorn-core:15.4")
+    // https://mvnrepository.com/artifact/commons-codec/commons-codec
+    implementation("commons-codec:commons-codec:1.20.0")
+    // https://mvnrepository.com/artifact/org.bouncycastle/bcprov-jdk15on
+    implementation("org.bouncycastle:bcprov-jdk15on:1.70")
+    // https://mvnrepository.com/artifact/com.google.guava/guava
+    implementation("com.google.guava:guava:33.5.0-jre")
+    // Source: https://mvnrepository.com/artifact/cn.iflow/iflow-cli-sdk
+    implementation("cn.iflow:iflow-cli-sdk:1.0.4")
+
+    testImplementation(platform("org.junit:junit-bom:5.14.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
