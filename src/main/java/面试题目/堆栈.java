e4e4b0b1a8c9c6c1cadb18b2c7aa04f3ce7a967d
chenpeigen <chenpeigen@xiaomi.com>
2026-02-07 21:36:25 +0800

    add 代码-lee

--- src/main/java/面试题目/堆栈.java
+++ src/main/java/面试题目/堆栈.java
@@ -1,7 +1,6 @@
 package 面试题目;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 
 public class 堆栈 {
 
@@ -93,7 +92,7 @@ public class 堆栈 {
      * 返回一个数组 answer ，其中 answer[i] 是指对于第 i 天，
      * 下一个更高温度出现在几天后。如果气温在这之后都不会升高，
      * 请在该位置用 0 来代替。
-     *
+     * <p>
      * 使用单调递减栈来解决此问题：
      * - 从右向左遍历温度数组
      * - 栈中存储的是温度的索引
@@ -116,16 +115,87 @@ public class 堆栈 {
             while (!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]) {
                 stack.pop();
             }
-            
+
             // 如果栈为空，说明后面没有更高的温度，ans[i]保持默认值0
             // 否则计算天数差：栈顶索引 - 当前索引
             ans[i] = stack.isEmpty() ? 0 : stack.peek() - i;
-            
+
             // 将当前索引入栈，为前面的元素提供比较基准
             stack.push(i);
         }
 
         return ans;
+    }
+
+
+    /**
+     * 查找数组中第k大的元素
+     * 使用最大堆来实现，时间复杂度为O(nlogn)
+     *
+     * @param nums 整数数组
+     * @param k    第k大的元素（k从1开始计数）
+     * @return 数组中第k大的元素
+     */
+    // https://leetcode.cn/problems/kth-largest-element-in-an-array/description/?envType=study-plan-v2&envId=top-100-liked
+    public int findKthLargest(int[] nums, int k) {
+        // 创建最大堆，比较器定义为(b - a)实现降序排列
+        PriorityQueue<Integer> queMin = new PriorityQueue<>((a, b) -> b - a);
+
+        // 将所有元素加入最大堆
+        for (int num : nums) {
+            queMin.offer(num);
+        }
+
+        // 弹出k-1个最大元素，剩下的堆顶就是第k大的元素
+        while (--k > 0) {
+            queMin.poll();
+        }
+
+        // 返回堆顶元素，即第k大的元素
+        return queMin.peek();
+    }
+
+    /**
+     * 找出数组中出现频率前k高的元素
+     * 使用哈希表统计频次，最小堆维护前k个高频元素
+     *
+     * @param nums 输入的整数数组
+     * @param k    需要返回的高频元素个数
+     * @return 包含前k个高频元素的数组
+     */
+    // https://leetcode.cn/problems/top-k-frequent-elements/submissions/697148018/?envType=study-plan-v2&envId=top-100-liked
+    public int[] topKFrequent(int[] nums, int k) {
+        // 使用HashMap统计每个数字出现的频次
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            // getOrDefault方法获取当前数字的频次，不存在则默认为0，然后加1
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // 创建最小堆，按照频次升序排列（堆顶是最小频次元素）
+        // 比较器(a,b)->b.getValue()-a.getValue()实现按频次降序排列
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(
+                (a, b) -> b.getValue() - a.getValue()
+        );
+
+        // 遍历频次映射，将元素加入堆中
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            pq.offer(entry);  // 将当前元素加入堆
+            // 如果堆大小超过k，则移除频次最小的元素（堆顶）
+            // 这样保证堆中始终只保留频次最高的k个元素
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        // 构造结果数组，从堆中取出前k个高频元素
+        int[] res = new int[k];
+        // 从后往前填充结果数组，因为堆顶是频次最小的元素
+        for (int i = k - 1; i >= 0; i--) {
+            res[i] = pq.poll().getKey();  // 取出键值（原数组中的数字）
+        }
+
+        return res;  // 返回前k个高频元素数组
     }
 
 }
@@ -157,5 +227,78 @@ class MinStack {
 
     public int getMin() {
         return minStack.peek();
+    }
+}
+
+/**
+ * MedianFinder 类用于动态维护数据流的中位数
+ * 使用两个优先队列（堆）来实现：
+ * - 最大堆 queMin：存储较小的一半元素
+ * - 最小堆 queMax：存储较大的一半元素
+ * 通过维护两个堆的大小平衡，可以在 O(1) 时间内获取中位数
+ */
+// 我们用两个优先队列 queMax 和 queMin 分别记录大于中位数的数和小于等于中位数的数。
+class MedianFinder {
+    // 最大堆，存储较小的一半元素，使用逆序比较器实现
+    PriorityQueue<Integer> queMin = new PriorityQueue<>(Collections.reverseOrder());
+
+    // 最小堆，存储较大的一半元素，使用默认升序比较器
+    PriorityQueue<Integer> queMax = new PriorityQueue<>();
+
+    /**
+     * 构造函数，初始化 MedianFinder 对象
+     */
+    public MedianFinder() {
+        // 初始化时两个堆都为空
+    }
+
+    /**
+     * 向数据结构中添加一个数字
+     * 保持两个堆的大小平衡：
+     * - 最大堆的大小最多比最小堆大1
+     * - 最小堆的大小不能超过最大堆
+     *
+     * @param num 要添加的整数
+     */
+    public void addNum(int num) {
+        // 可以比做一个漏斗的形状
+        // 如果最大堆为空，或者num小于等于最大堆的堆顶，则加入最大堆
+        if (queMin.isEmpty() || num <= queMin.peek()) {
+            queMin.offer(num);
+
+            // 6 ==4 ==》 5 == 5
+            // 平衡操作：如果最大堆的大小超过最小堆大小+1，
+            // 则将最大堆堆顶移到最小堆，保持大小平衡
+            if (queMin.size() > queMax.size() + 1) {
+                queMax.offer(queMin.poll());
+            }
+        } else {
+            // 否则加入最小堆
+            queMax.offer(num);
+
+            // 平衡操作：如果最小堆的大小超过最大堆，
+            // 则将最小堆堆顶移到最大堆，保持大小平衡
+            // 5 4  ==> 4 5
+            if (queMax.size() > queMin.size()) {
+                queMin.offer(queMax.poll());
+            }
+        }
+    }
+
+    /**
+     * 获取当前数据流的中位数
+     *
+     * @return 当前数据流的中位数
+     */
+    public double findMedian() {
+        // 如果两个堆大小相等，说明总元素个数为偶数
+        // 中位数为两个堆顶元素的平均值
+        if (queMin.size() == queMax.size()) {
+            return (queMin.peek() + queMax.peek()) / 2.0;
+        } else {
+            // 如果两个堆大小不等，说明总元素个数为奇数
+            // 中位数为较大堆（最大堆）的堆顶元素
+            return queMin.peek();
+        }
     }
 }
